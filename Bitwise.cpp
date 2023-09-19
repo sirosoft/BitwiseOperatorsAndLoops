e85fbd48d64b7272f10c2addc4a05fe20b99ca1a
Yuvraj <yuvrajrawat316@gmail.com>
2023-09-20 01:28:32 +0530

    commit

---
+++ Bitwise.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    // int a = 5;
+    // int b = 10;
+
+    // cout << (a&b) << endl;
+    // cout << (2|3) << endl;
+
+    // bool num = 1;
+    // num = ~num;
+
+    // cout<<num<<endl;
+
+    // cout << (~num) <<endl;
+
+    // int a = 5;
+    // int b = -5;
+
+    // cout<< (a^b) <<endl;
+
+
+    // int a = 7;
+    // int ans = a<<10;
+    // cout << ans << endl;
+
+    unsigned int a = -805306368;
+    cout << (a<<3) << endl;
+
+
+
+
+
+    return 0;
+}
